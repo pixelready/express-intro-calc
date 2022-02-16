d3fc33929c7e32170b8ec0086eb12ea7cd00cf81
pixelready <bizartist@gmail.com>
2022-02-16 15:15:48 -0800

    working calc routes

--- app.js
+++ app.js
@@ -4,19 +4,38 @@ const express = require("express");
 const app = express();
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const {NotFoundError, BadRequestError} = require("./expressError");
+const {convertStrNums} = require("./utils");
+const {findMean, findMedian, findMode} = require("./stats");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
-
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
-
+app.get("/mean", function (req, res) {
+  if (req.query.nums === undefined) {
+    throw new BadRequestError("nums are required");
+  }
+  const nums = convertStrNums(req.query.nums);
+  return res.send({operation: "mean", value: findMean(nums)});
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res) {
+  if (req.query.nums === undefined) {
+    throw new BadRequestError("nums are required");
+  }
+  const nums = convertStrNums(req.query.nums);
+  return res.send({operation: "median", value: findMedian(nums)});
+});
 
-
-/** Finds mode of nums in qs: returns {operation: "mean", result } */
-
+/** Finds mode of nums in qs: returns {operation: "mode", result } */
+app.get("/mode", function (req, res) {
+  if (req.query.nums === undefined) {
+    throw new BadRequestError("nums are required");
+  }
+  const nums = convertStrNums(req.query.nums);
+  return res.send({operation: "mode", value: findMode(nums)});
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
@@ -27,10 +46,14 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   const status = err.status || 500;
   const message = err.message;
-  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
-  return res.status(status).json({ error: { message, status } });
+  if (process.env.NODE_ENV !== "test") 
+    console.error(status, err.stack);
+  return res.status(status).json({
+    error: {
+      message,
+      status
+    }
+  });
 });
-
-
 
 module.exports = app;
--- utils.js
+++ utils.js
@@ -1,12 +1,22 @@
-const { BadRequestError } = require("./expressError");
-
+const {BadRequestError} = require("./expressError");
 
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
 function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+
+  let strNumsArray = strNums.split(",");
+  numsArray = strNumsArray.map(numStr => Number(numStr));
+  for (let i = 0; i < numsArray.length; i++) {
+    if (Number.isNaN(numsArray[i])) {
+      throw new BadRequestError(`${strNumsArray[i]} is not a number!`);
+    }
+  }
+  console.log("numsArray ", numsArray);
+  return numsArray;
 }
 
-
-module.exports = { convertStrNums };
+module.exports = {
+  convertStrNums
+};
